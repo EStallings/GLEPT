4089900ad9b4b762b293a6179763fac5f6eadb65
unknown <martin.lidy@ymail.com>
2014-11-14 13:37:29 -0700

    libs again

---
+++ GLEPT/source/pathtracer/main.cpp
@@ -0,0 +1,457 @@
+#include <math.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+// OpenGL
+//#include <GL/gl.h>
+#define GLEW_STATIC
+#include "glew.h"
+#include <GL/glut.h>
+
+// Geometry
+#include "objLoader.h"
+#include "obj_parser.h"
+using namespace std;
+
+GLuint   program_object;
+GLuint	 program_object2;
+GLuint   vertex_shader;
+GLuint   fragment_shader;
+
+GLuint framebufferID;// = glGenFramebuffersEXT();											// create a new framebuffer
+GLuint colorTextureID;// = glGenTextures();												// and a new texture used as a color buffer
+
+//
+double newtime, oldtime;
+
+// Log information
+static void printProgramInfoLog(GLuint obj)
+{
+	GLint infologLength = 0, charsWritten = 0;
+	glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
+	if (infologLength > 2) {
+		GLchar* infoLog = new GLchar [infologLength];
+		glGetProgramInfoLog(obj, infologLength, &charsWritten, infoLog);
+		std::cerr << infoLog << std::endl;
+		delete infoLog;
+	}
+}
+
+// read a file to string
+std::string readFile(const char *filePath) {
+	std::string content;
+	std::ifstream fileStream(filePath, std::ios::in);
+
+	if (!fileStream.is_open()) {
+		std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+		return "";
+	}
+
+	std::string line = "";
+	while (!fileStream.eof()) {
+		std::getline(fileStream, line);
+		content.append(line + "\n");
+	}
+	fileStream.close();
+	return content;
+}
+
+
+float *VertsToFloat3(obj_vector** verts, int vertCount){
+	int arraySize = vertCount * 3;
+	float *output = (float*)malloc(arraySize * sizeof(float));
+
+	// Each vert
+	for (int i = 0; i < vertCount; i++){
+		// Each coord
+		for (int k = 0; k < 3; k++){
+			//printf("%f", verts[i]->e[k]);
+			output[i * 3 + k] = verts[i]->e[k];
+		}
+	}
+
+	return output;
+}
+
+float *GetObjectMaterials(objLoader* object, int faceCount){
+	int arraySize = object->materialCount * 3;
+	float *materials = (float*)malloc(arraySize * sizeof(float));
+
+
+	// Each material
+	for (int i = 0; i < object->materialCount; i++){
+
+		// Each color channel
+		for (int k = 0; k < 3; k++){
+			materials[i * 3 + k] = object->materialList[i]->diff[k];
+			//materials[i * 3 + k] = object->materialList[object->faceList[i]->material_index]->diff[k];
+		}
+	}
+
+	return materials;
+}
+
+static double* getFaceNormals(objLoader objData, int faceCount){
+	/*int arraySize = objData->normalCount * 3;
+	double *normals = (double*)malloc(arraySize * sizeof(double));
+
+	for (int i = 0; i < faceCount; i++){
+	for (int k = 0; k < 3; k++){
+	normals[3*i + k] = objData->normalList[3 * (objData->faceList[i]->normal_index) + k];
+	}
+	}*/
+}
+
+int *FacesToMats(objLoader* object, int faceCount){
+	int arraySize = faceCount;
+	int *faceMats = (int*)malloc(arraySize * sizeof(int));
+
+	// Each face
+	for (int i = 0; i < faceCount; i++){
+		faceMats[i] = object->faceList[i]->material_index;
+	}
+
+	return faceMats;
+}
+
+int *FacesToVerts(obj_face** faces, int faceCount){
+	int arraySize = faceCount * 3;
+	int *output = (int*)malloc(arraySize * sizeof(int));
+
+	// Each face
+	for (int i = 0; i < faceCount; i++){
+
+		// Each vert
+		for (int k = 0; k < 3; k++){
+			output[i * 3 + k] = faces[i]->vertex_index[k];//printf("   Vert: %d", output[i * 3 + k]);
+		}
+	}
+
+	return output;
+}
+
+objLoader * parseObj(){
+	objLoader *objData = new objLoader();
+	objData->load("test.obj");
+
+	//
+	const int faceAmount = objData->faceCount;
+
+	printf("Number of vertices: %i\n", objData->vertexCount);
+	printf("Number of faces: %i\n", objData->faceCount);
+	printf("Number of materials: %i\n", objData->materialCount);
+	printf("\n");
+
+	printf("Number of faces: %i\n", objData->faceCount);
+	printf("Material List: %d\n", objData->materialList[objData->faceList[0]->material_index]->diff);
+
+	return objData;
+}
+
+
+// Create the vertex and fragment shaders
+GLuint LoadShader(const char *vertex_path, const char *fragment_path) {
+	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Read shaders
+	std::string vertShaderStr = readFile(vertex_path);
+	std::string fragShaderStr = readFile(fragment_path);
+	const char *vertShaderSrc = vertShaderStr.c_str();
+	const char *fragShaderSrc = fragShaderStr.c_str();
+
+	GLint result = GL_FALSE;
+	int logLength;
+
+	// Compile vertex shader
+	std::cout << "Compiling vertex shader." << std::endl;
+	glShaderSource(vertShader, 1, &vertShaderSrc, NULL);
+	glCompileShader(vertShader);
+
+	// Check vertex shader
+	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
+
+
+	// Compile fragment shader
+	std::cout << "Compiling fragment shader." << std::endl;
+	//std::cout << fragShaderSrc << std::endl;
+	glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
+	glCompileShader(fragShader);
+	
+	GLint compiled;
+	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &compiled);
+	//mada_check_gl_error();
+	if (!compiled)
+	{
+		GLint length;
+		glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &length);
+		//mada_check_gl_error();
+		if (length > 0)
+		{
+			GLint infoLength;
+			char* infoBuf = (char*)malloc(sizeof(char)* length);
+			glGetShaderInfoLog(fragShader, length, &infoLength, infoBuf);
+			printf("ERROR: %s", infoBuf);
+			//mada_check_gl_error();
+			//mada_log(logERROR, infoBuf);
+			//SysUtils::error("Error compiling shader. See log for info.");
+			free(infoBuf);
+		}
+		//SysUtils::error("Failed to compile shader. No further info available.");
+	}
+
+	// Check fragment shader
+	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &logLength);
+
+	std::cout << "Linking program" << std::endl;
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertShader);
+	glAttachShader(program, fragShader);
+	glLinkProgram(program);
+
+	printProgramInfoLog(program);   // verifies if all this is ok so far
+
+	glGetProgramiv(program, GL_LINK_STATUS, &result);
+	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+	std::vector<char> programError((logLength > 1) ? logLength : 1);
+	glGetProgramInfoLog(program, logLength, NULL, &programError[0]);
+	std::cout << &programError[0] << std::endl;
+
+	glDeleteShader(vertShader);
+	glDeleteShader(fragShader);
+
+	return program;
+}
+
+
+// Our GL INITS
+bool init(void){    
+   glClearColor(0.7f, 0.7f, 0.7f, 0.5f);	// Black Background
+
+   program_object2 = LoadShader("VertexShader.vert", "FragmentShader2.frag");
+
+   program_object = LoadShader("VertexShader.vert", "FragmentShader.frag");
+   glUseProgram(program_object);
+
+   /* PARSING OBJECTS BITCHES */
+   std::cout << "Starting to send memory: " << std::endl;
+   objLoader* loadedObject = parseObj();
+
+   // export verts
+   float* vertArray = VertsToFloat3(loadedObject->vertexList, loadedObject->vertexCount);
+
+   // export faces
+   int* faceArray = FacesToVerts(loadedObject->faceList, loadedObject->faceCount);
+   float* materials = GetObjectMaterials(loadedObject, loadedObject->faceCount);
+   int* faceMats = FacesToMats(loadedObject, loadedObject->faceCount);
+
+   //create texture A
+   glEnable(GL_TEXTURE_2D);
+   glGenTextures(1, &colorTextureID);
+   glBindTexture(GL_TEXTURE_2D, colorTextureID);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 512, 512, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+
+	//create fboA and attach texture A to it
+	glGenFramebuffersEXT(1, &framebufferID);
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, framebufferID);
+	glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_TEXTURE_2D, colorTextureID, 0);
+	
+	GLenum status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+	if (status != GL_FRAMEBUFFER_COMPLETE_EXT)
+		printf("NOT COMPLETE!!!!\n\n");
+
+	printf("GL_VERSION:%s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	glUniform1fv(glGetUniformLocation(program_object, "verts"), loadedObject->vertexCount*3, vertArray);
+	glUniform1iv(glGetUniformLocation(program_object, "faces"), loadedObject->faceCount*3, faceArray);
+	glUniform1iv(glGetUniformLocation(program_object, "faceMat"), loadedObject->faceCount, faceMats);
+	glUniform1fv(glGetUniformLocation(program_object, "Materials"), loadedObject->materialCount*3, materials);
+	glUniform1i(glGetUniformLocation(program_object, "faceCount"), loadedObject->faceCount);
+	std::cout << "Finished sending memory: FaceCount:" << loadedObject->faceCount << std::endl;
+
+
+	// -----------------------------------------//
+	// GEOMETRY //
+	GLuint blockIndex = glGetUniformBlockIndex(program_object, "BlobSettings");
+	GLint blockSize;
+
+	glGetActiveUniformBlockiv(program_object, blockIndex,GL_UNIFORM_BLOCK_DATA_SIZE, &blockSize);
+
+	GLubyte * blockBuffer = (GLubyte *)malloc(blockSize);
+	
+	// Query for the offsets of each block variable
+	const GLchar *names[] = { "InnerColor", "OuterColor",
+                          "RadiusInner", "RadiusOuter" };
+
+	GLuint indices[4];
+	glGetUniformIndices(program_object, 4, names, indices);
+
+	GLint offset[4];
+	glGetActiveUniformsiv(program_object, 4, indices,GL_UNIFORM_OFFSET, offset);
+
+	//Place the data into the buffer at the appropriate offsets.
+	GLfloat outerColor[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+	GLfloat innerColor[] = { 1.0f, 1.0f, 0.75f, 1.0f };
+	GLfloat innerRadius = 0.25f, outerRadius = 0.45f;
+
+	memcpy(blockBuffer + offset[0], innerColor,
+		4 * sizeof(GLfloat));
+	memcpy(blockBuffer + offset[1], outerColor,
+		4 * sizeof(GLfloat));
+	memcpy(blockBuffer + offset[2], &innerRadius,
+		sizeof(GLfloat));
+	memcpy(blockBuffer + offset[3], &outerRadius,
+		sizeof(GLfloat));
+
+	//Create the OpenGL buffer object and copy the data into it.
+	GLuint uboHandle;
+
+	glGenBuffers(1, &uboHandle);
+	glBindBuffer(GL_UNIFORM_BUFFER_EXT, uboHandle);
+	glBufferData(GL_UNIFORM_BUFFER_EXT, blockSize, blockBuffer,
+		GL_DYNAMIC_DRAW);
+
+	// Bind the buffer object to the uniform block.
+	glBindBufferBase(GL_UNIFORM_BUFFER_EXT, blockIndex, uboHandle);
+
+	return true;
+}
+
+// Our rendering is done here
+void render(void)  {
+	float scale = 5.00f;
+	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	// Camera crap
+	gluLookAt(0.0, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	//glViewport(0, 0, 512, 512);
+
+	//
+	//Create FBO
+	glGenFramebuffersEXT(1, &framebufferID);
+
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0); // switch to rendering on our FBO
+	glActiveTexture(GL_TEXTURE0); //make texture register 0 active
+	glBindTexture(GL_TEXTURE0, colorTextureID); //bind textureA as out input texture
+
+	//glClearColor(0.0f, 1.0f, 0.0f, 0.5f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);            // Clear Screen And Depth Buffer on the fbo to red
+
+	// Render to FBO
+	glUseProgram(program_object);
+	glActiveTexture(GL_TEXTURE_2D); //make texture register 0 active
+	glBindTexture(GL_TEXTURE_2D, colorTextureID); //bind textureA as out input texture
+
+	////glUniform1i(GL_TEXTURE0, 0); //pass texture B as a sampler to the shader
+	GLuint location = glGetUniformLocation(program_object, "colorTex");
+	glUniform1i(location, colorTextureID);
+	glUniform1i(glGetUniformLocation(program_object, "randomSeed"), rand());
+
+	glBegin(GL_QUADS);
+	glVertex3f(-0.5*scale, -0.5*scale, 0.0);
+	glVertex3f(0.5*scale, -0.5*scale, 0.0);
+	glVertex3f(0.5*scale, 0.5*scale, 0.0);
+	glVertex3f(-0.5*scale, 0.5*scale, 0.0);
+	glEnd();
+
+	glUseProgram(0);
+
+	// DISPLAY ON SCREEN
+	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 1);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(program_object2);
+	glActiveTexture(GL_TEXTURE0); //make texture register 0 active
+	glBindTexture(GL_TEXTURE0, colorTextureID); //bind either textureA
+
+	location = glGetUniformLocation(program_object2, "texture");
+	glUniform1i(location, GL_TEXTURE0);
+
+	glBegin(GL_QUADS);
+		glVertex3f(-0.5*scale, -0.5*scale, 0.0);
+		glVertex3f(0.5*scale, -0.5*scale, 0.0);
+		glVertex3f(0.5*scale, 0.5*scale, 0.0);
+		glVertex3f(-0.5*scale, 0.5*scale, 0.0);
+	glEnd();
+
+	glUseProgram(0);
+
+	// Swap The Buffers To Make Our Rendering Visible
+	glutSwapBuffers();
+	//SwapBuffers();
+
+	// Timer
+	//newtime = glewgettime
+	//double difference = (newtime - oldtime)/1000;
+	//oldtime = newtime;
+	//std::cout << "Time: " << difference << std::endl;
+}
+
+// Our Reshaping Handler (Required Even In Fullscreen-Only Modes)
+void reshape(int w, int h){
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);     // Select The Projection Matrix
+	glLoadIdentity();                // Reset The Projection Matrix
+	
+	// Calculate The Aspect Ratio And Set The Clipping Volume
+	if (h == 0) h = 1;
+	gluPerspective(90, (float)w/(float)h, 0.1, 100.0);
+	glMatrixMode(GL_MODELVIEW);      // Select The Modelview Matrix
+	glLoadIdentity();                // Reset The Modelview Matrix
+
+	// Variables
+	GLint loc = glGetUniformLocation(program_object, "iResolution");
+	if (loc != -1){
+		std::cout << "Setting uniform variables" << std::endl;
+		glUniform2f(loc, w, h);
+	}
+
+	float test[982] = {1.0};
+}
+
+// Our keyboard handler
+void keyboard(unsigned char key, int x, int y){
+	switch (key) {
+		case 27:       // When escape is pressed...
+			//exit(0);    // Exit The Program
+		   break;      
+		default:       
+		break;
+	}
+}
+
+// Main Function For Bringing It All Together.
+int main(int argc, char** argv){	
+	glutInit(&argc, argv);                           // GLUT Initializtion
+	//glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);     // Display Mode (Rgb And Double Buffered)	
+	glutCreateWindow("GLSL PathTracer V0.01");       // Window Title 
+
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		/* Problem: glewInit failed, something is seriously wrong. */
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+	}
+	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+	
+	init();                                          // Our Initialization
+	glutDisplayFunc(render);                         // Register The Display Function
+	glutReshapeFunc(reshape);                        // Register The Reshape Handler
+	glutKeyboardFunc(keyboard);                      // Register The Keyboard Handler
+	glutIdleFunc(render);                            // Do Rendering In Idle Time
+	glutMainLoop();                                  // Go To GLUT Main Loop
+	return 0;
+}
---
+++ GLEPT/source/shaders/FragmentShader.frag
@@ -0,0 +1,386 @@
+#define FaceCount 1023
+#define PI 3.14
+
+//layout(location = 0) out vec3 colorTex;
+
+uniform vec2 iResolution;
+uniform float verts[610*3];
+uniform int faces[650*2];
+uniform int faceCount;
+uniform int faceMat[900];
+uniform float Materials[7*4];
+uniform int randomSeed;
+uniform int sampleNumber;
+uniform sampler2D lastFrame;
+
+// -- CONSTANTS --//
+
+// Lights
+vec3 LPos = vec3(15.0,-15.0,10.0);
+
+struct Light
+{
+  vec3 color;
+  vec3 position;
+  float radius;
+  float brightness;
+};
+
+const int MAX_LIGHTS = 1;
+Light L2 = Light(vec3(1.0),vec3(15.0,-15.0,10.0),0.75,1.0);
+Light L1 = Light(vec3(1.0),vec3(1.0,-7.0,9.0), 0.5, 1.0);
+
+Light[2] LIGHTS = {L1,L2};
+
+vec3 ambientLight = vec3(0.0,0.0,0.0);
+
+// Camera //
+float FOV = 0.95f;
+vec3 camPos = vec3(1.0,-13.0,5.0);
+vec3 forward = normalize(vec3(0.0,1.0,0.0));
+vec3 up      = normalize(vec3(0.0,0.0,1.0));
+vec3 right = normalize(cross(forward, up));
+
+
+// HDR tonemapping
+vec3 tonemapping(vec3 color){
+	float bias = 0.1;
+	float max = length(color);
+	color = color/(max - bias);
+
+	return color;
+}
+
+
+// Random float
+float rand(vec2 co, float min, float max){
+    return min + (max-min)*fract(sin(dot(co.xy*randomSeed ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+float rand2() {
+	return fract(sin(randomSeed)*43758.5453123);
+}
+
+vec3 jitter(vec3 d, float phi, float sina, float cosa) {
+	vec3 w = normalize(d), u = normalize(cross(w.yzx, w)), v = cross(w, u);
+	return (u*cos(phi) + v*sin(phi)) * sina + w * cosa;
+}
+
+// Floor plane
+bool plane(vec3 pos, vec3 norm, vec3 ro, vec3 rd, inout vec3 hit, inout float dist)
+{
+	dist = dot(pos-ro, norm) / dot(rd, norm);
+	
+	if(dist < 0.00000001){ 
+		return false;
+	}
+	hit = ro + dist * rd;
+	return true;
+}
+
+// Implicit Sphere
+vec4 sphere(vec3 ray, vec3 dir, vec3 center, float radius)
+{
+	vec3 rc = center;
+	float c = dot(rc, rc) - (radius*radius);
+	float b = dot(dir, rc);
+	float d = b*b - c;
+	float t = -b - sqrt(abs(d));
+	float st = step(0.0f, min(t,d));
+	vec4 color = vec4(0);
+
+	vec3 hit = ray + dir*vec3(t);
+	vec3 N = -1.0f * normalize(vec3((hit.x - center.x)/radius, (hit.y - center.y)/radius, (hit.z - center.z)/radius));
+
+	// Return lit sphere
+	return vec4(1.0 - clamp(-1.0 + (1.0 + t)*d, 0.0, 1.0));
+}
+
+// get soft shadow Point Light location
+vec3 getLightPos(int index){
+	LPos = LIGHTS[index].position;//LIGHTS[index].position;
+	vec3 softLPos = LPos - vec3(rand(gl_FragCoord.xy ,-LIGHTS[index].radius,LIGHTS[index].radius));
+	return softLPos;
+}
+
+// Point lighting
+vec3 lightFace(vec3 N, vec3 Pos){
+	int i = 0;
+	vec3 sum = vec3(0.0);
+
+	// Each light
+	for(i=0;i<MAX_LIGHTS;i++){
+		vec3 softLPos = getLightPos(i);
+
+		// Light attenuation //
+		float Ldist = length(softLPos - Pos);
+		float a = 0.01;
+		float b = 0.001;
+		float att = 1.0 / (1.0 + a*Ldist + b*Ldist*Ldist);
+
+		sum += clamp(dot(N, normalize(softLPos - Pos)) * att,0.0f,1.0f) * LIGHTS[i].color * LIGHTS[i].brightness;
+	}
+	
+	return sum;
+}
+
+// Geometry
+bool triangle(vec3 v0, vec3 v1, vec3 v2, vec3 ro, vec3 rd, inout vec3 hit, inout float dist, inout vec3 norm)
+{
+	vec3 cubePos = vec3(0.0,-5.0,0.0);
+	v0 = cubePos + v0;
+	v1 = cubePos + v1;
+	v2 = cubePos + v2;
+
+	vec3 edge1 = v2 - v0;
+	vec3 edge2 = v1 - v0;
+
+	vec3 pvec = cross(rd, edge2);
+
+	float det = dot(edge1, pvec);
+
+	if (det == 0) return false;
+
+	float invDet = 1 / det;
+	vec3 tvec = ro - v0;
+
+	float u = dot(tvec, pvec) * invDet;
+
+	if (u < 0 || u > 1) return false;
+
+	vec3 qvec = cross(tvec, edge1);
+	float v = dot(rd, qvec) * invDet;
+
+	if (v < 0 || u + v > 1) return false;
+
+	dist = dot(edge2, qvec) * invDet;
+	hit = ro + rd* (dist);
+	norm = vec3( edge1.y*edge2.z - edge1.z*edge2.y, edge1.z*edge2.x - edge1.x*edge2.z, edge1.x*edge2.y - edge1.y*edge2.x);
+	norm = normalize(norm);
+
+	if (dot(norm, normalize(camPos)) < 0.0001f){	
+		norm = -1.0f * norm;
+	}
+
+	return true;
+}
+
+
+// Find intersecting face
+int getIntersection(vec3 rayOrigin, vec3 rayDir, inout vec3 hit2, inout vec3 norm2){
+	vec3 v1,v2,v3;
+	vec3 minHit, minNorm;
+	float minDist = 999999.0;
+	int k = 0;
+
+	vec3 hit;
+	float dist;
+	vec3 norm;
+
+	int hitFaceIndex = -1;
+
+	// For each face in faces array
+	for(k=0; k<faceCount; k++){
+		v1 = vec3( verts[3*faces[3*k]+0],	verts[3*faces[3*k]+1],		verts[3*faces[3*k]+2] );
+		v2 = vec3( verts[3*faces[3*k+1]+0],	verts[3*faces[3*k+1]+1],	verts[3*faces[3*k+1]+2] );
+		v3 = vec3( verts[3*faces[3*k+2]+0],	verts[3*faces[3*k+2]+1],	verts[3*faces[3*k+2]+2] );
+
+		// Colision check
+		if(triangle(v1, v2, v3, rayOrigin, rayDir, hit, dist, norm)){
+			if(dist < minDist){
+				minDist = dist;
+				minHit = hit;
+				minNorm = norm;
+				hitFaceIndex = k;
+			}
+		}
+	}
+
+	hit2 = minHit;
+	norm2 = minNorm;
+
+	return hitFaceIndex;
+}
+
+vec3 getPointColor( int faceIndex ){
+	
+	// obj is the floor
+	if (faceIndex == -1){
+		return vec3(1.0,0.9,0.9);
+	}
+
+	return vec3( Materials[3*faceMat[faceIndex]+0], Materials[3*faceMat[faceIndex]+1], Materials[3*faceMat[faceIndex]+2] );
+}
+
+vec3 bounceRay( vec3 rayPos, vec3 rayDir, int depth){
+	int MAXDEPTH = 3;
+
+	vec3 norm =  vec3(0.0);
+	vec3 hit  =  vec3(0.0);
+	vec3 point_color = vec3(0.0);
+	int objIndex;
+	
+	// Recursion hack
+	while(depth <= MAXDEPTH){
+		depth += 1;
+
+		// Monte carlo
+		vec3 l0 = hit - LIGHTS[0].position;
+		float cos_a_max = sqrt(1. - clamp(LIGHTS[0].radius * LIGHTS[0].radius / dot(l0, l0), 0., 1.));
+		float cosa = mix(cos_a_max, 1., rand2());
+
+		vec3 l = jitter(l0, 2.0*PI*rand2(), sqrt(1.0 - cosa*cosa), cosa);
+		//rayDir = hit - l;
+		
+		// Check ray intersection
+		objIndex = getIntersection( rayPos, normalize(rayDir), hit, norm);
+
+		// Did hit geometry
+		if(objIndex != -1){
+			point_color += getPointColor(objIndex) * lightFace(norm, hit) * 1/depth;
+			rayPos = hit;
+			rayDir = norm;
+		}
+
+		// cancel the iterations
+		else{
+			depth = MAXDEPTH + 1;
+		}
+	}
+
+	return point_color;
+}
+
+vec3 traceRay( vec3 rayPos, vec3 rayDir, int currentBounce)
+{
+	int MAX_BOUNCE = 1;
+
+	vec3 reflect_color = vec3(0.0);
+	vec3 refract_color = vec3(0.0);
+	vec3 bounce_color = vec3(0.0);
+
+	vec3 point_color = vec3(0.0);
+	vec3 hit, norm, minHit, minNorm,norm2;
+	vec3 shadowhit;
+	vec3 sum = vec3(0.0);
+	float dist;
+
+	int objIndex;
+	bool hitCube = false;
+
+	// Check ray intersection
+	objIndex = getIntersection( rayPos, rayDir, hit, norm);
+
+	// Did hit geometry
+	if(objIndex != -1){
+		point_color += getPointColor(objIndex) * (lightFace(norm, hit) + ambientLight);
+	}
+
+	// Hit the floor
+	else if(plane( vec3(0.0), normalize(vec3(0.0,0.0,1.0)), rayPos, rayDir, hit, dist))
+	{
+		norm = normalize(vec3(0.0,0.0,1.0));
+
+		// Plane stuff
+		float scale = 0.4;
+
+		//do this calculation for all x, y, z, and it will work regardless of normal
+		if ( mod( round( abs(hit.x)*scale) + round(abs(hit.y)*scale) + round(abs(hit.z)*scale), 2.0f) < 1.0){
+			point_color += lightFace(vec3(0.0,0.0,1.0), hit);
+		}	
+		else{
+			point_color =+ vec3(1.0,0.0,0.0) * lightFace(vec3(0.0,0.0,1.0), hit);
+		}
+	}
+	
+	// SHADOWS //
+	int i = 0;
+	int k = 0;
+
+	int SHADOW_SAMPLES = 1;
+	
+	// for each light
+	for(i=0;i<MAX_LIGHTS;i++){
+
+		// samples
+		for(k=0;k<SHADOW_SAMPLES;k++){
+			vec3 softLPos = getLightPos(i);
+			bool isShadow = false;
+			int shadowIndex = -1;
+			vec3 shadowRayDir = normalize(softLPos-hit);
+			vec3 shadowRayPos = hit;
+
+			float distToLight = length(softLPos - hit);
+
+			// need a faster method, it can break the loop as soon as
+			// it finds an intersection.  we dont need the closest
+			shadowIndex = getIntersection( shadowRayPos, -shadowRayDir, shadowhit, norm2);
+
+			// Hits another object
+			if (shadowIndex != -1){
+		
+				// if the intersection is before or after the light 
+				float l = length(hit - shadowhit);
+
+				if( (l>0.0001) && (l < distToLight) ){
+					isShadow = true;
+				}
+			}
+
+			// --
+			bounce_color = bounceRay(hit, norm,0);
+
+			if (isShadow){
+				point_color = point_color*(bounce_color + vec3(ambientLight));
+			}
+			else{
+				point_color += bounce_color*0.1;
+			}
+		}
+	}
+
+	return point_color;
+}
+
+void main()
+{
+	vec3 point_color;
+	vec3 hit;
+	vec3 norm;
+	float dist;
+
+	// MSAA //
+	int i = 0;
+	int k = 0;
+	float rx = 0.0;
+	float ry = 0.0;
+	int samples = 1;
+	float AA_amount = 0.05;
+
+	// Geometry //
+	vec4 sum = vec4(0.0f);
+	
+	//
+	float minDist = 10000000;
+	bool hitCube = false;
+
+	// Screen info //
+	const vec2 iResolution = vec2(512,512);
+	vec3 screenCoords = vec3( (gl_FragCoord.xy / iResolution.xy)*2.0 - 1.0,0);
+
+	// --
+	vec3 rayOrigin = camPos + forward;
+	vec3 rayDir = normalize(screenCoords.x*right + screenCoords.y*up + forward * FOV);
+
+	// GEOMETRY
+	sum.xyz = traceRay(rayOrigin, rayDir, 0);
+	
+	// Lit Sphere
+	//sum += sphere( rayOrigin, rayDir, LPos, 0.001f);
+
+	// Save frame
+	//colorTex.rgb = sum.rgb;
+	gl_FragColor.rgb = sum.rgb;
+}
+
+//sum = (last*samples + current)/(samples+1);
